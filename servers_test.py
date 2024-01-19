2b23ab1eb744426a08bfb4c5bb76d02a2da2ea44
Jakub Gajewski <jgajewski@student.agh.edu.pl>
2024-01-19 20:48:51 +0100

    Gotowe

--- servers_test.py
+++ servers_test.py
@@ -9,26 +9,26 @@ server_types = (ListServer, MapServer)
 class ServerTest(unittest.TestCase):
 
     def test_get_entries_returns_proper_entries(self):
-        products = [Product('P12', 1), Product('PP234', 2), Product('PP235', 1)]
+        products = [Product('D12', 1), Product('DF123', 2), Product('DF234', 5)]
         for server_type in server_types:
             server = server_type(products)
             entries = server.get_entries(2)
             self.assertEqual(Counter([products[2], products[1]]), Counter(entries))
 
     def test_exceeding_n_max_returned_entries(self):
-        products = [Product('PS12', 1), Product('PP234', 2), Product('PS235', 5), Product('PD245', 9),
-                    Product('OK23', 1.5), Product('KP235', 6), Product('PW25', 2.5), Product('ZZ111', 4)]
+        products = [Product('DF12', 1), Product('DF123', 2), Product('DG123', 5), Product('DG234', 9),
+                    Product('OK23', 1.5), Product('KP235', 6), Product('DF45', 2.5), Product('HH333', 4)]
         for server_type in server_types:
             server = server_type(products)
             with self.assertRaises(TooManyProductsFoundError):
                 server.get_entries(2)
 
     def test_returned_list_sorted(self):
-        products = [Product('PS15', 4), Product('PP636', 3.5), Product('FS235', 5)]
+        products = [Product('DR12', 1), Product('DF123', 4), Product('DF234', 2)]
         for server_type in server_types:
             server = server_type(products)
             entries = server.get_entries(2)
-            self.assertEqual(entries, [Product('PP636', 3.5), Product('PS15', 4), Product('FS235', 5)])
+            self.assertEqual(entries, [Product('DR12', 1), Product('DF234', 2), Product('DF123', 4)])
 
 
 class ClientTest(unittest.TestCase):
